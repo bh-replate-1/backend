daa345b8887fd2443ba83707afaffc4522423c53
RohithSachdeva <rsachdeva@ucdavis.edu>
2020-08-26 16:28:36 -0700

    Few tests added before pulling master

--- auth/authTest.spec.js
+++ auth/authTest.spec.js
@@ -14,22 +14,22 @@ describe("Auth Router Testing", () => {
     it("Successful register returns a token", async () => {
         const res = await request(server)
         .post('/api/auth/register')
-        .send({ email: "Replate1", password: "Test" })
-        expect(res.body).toHaveProperty("message");
+        .send({ email: "Replate15", password: "Test" })
+        expect(res.body).toHaveProperty("token");
     });
 
     it("Successful register returns correct status code", async () => {
         const res = await request(server)
         .post('/api/auth/register')
-        .send({ email: "Replate2", password: "Test" })
-        expect(res.status).toBe(200);
+        .send({ email: "Replate16", password: "Test" })
+        expect(res.status).toBe(201);
     })
 
     it("Successful login returns a token", async () => {
         const res = await request(server)
         .post('/api/auth/login')
-        .send({ email: "Replate1", password: "Test" })
-        expect(res.body).toHaveProperty("message");
+        .send({ email: "Replate5", password: "Test" })
+        expect(res.body).toHaveProperty("token");
     })
     
     it("Invalid credentials return correct status code", async () => {
@@ -42,7 +42,7 @@ describe("Auth Router Testing", () => {
     it("Successful login returns correct status code", async () => {
         const res = await request(server)
         .post('/api/auth/login')
-        .send({ email: "Replate2", password: "Test" })
+        .send({ email: "Replate5", password: "Test" })
         expect(res.status).toBe(200);
     })
 })
--- food/foodTest.spec.js
+++ food/foodTest.spec.js
@@ -1,3 +1,47 @@
 const request = require("supertest");
 const server = require("../api/server.js");
 const db = require("../data/dbConfig.js");
+
+//get
+//getById
+//put .. id
+//delete .. id 
+
+
+// let token;
+// beforeAll((done) => {
+//     request(server)
+//       .post('/api/auth/login')
+//       .send({ email: "Replate15", password: "Test" })
+//       .end((err, response) => {
+//         token = response.body.token; // save the token!
+//         done();
+//       });
+//   });
+
+    
+
+
+
+
+
+
+describe("Food Router Testing", () => {
+    it("Returns correct unauthorized status code", () => {
+        return request(server).get('/api/food')
+        .then(data => {
+            expect(data.status).toEqual(401)
+        })
+    });
+
+    it("It responds with JSON", async () => {
+        const login = await request(server)
+        .post("/api/auth/login")
+        .send({ email: "Replate5", password: "Test" })
+        const res = await request(server)
+        .get('/api/food')
+        .set('Authorization', login.body.token)
+        expect(res.status).toEqual(200)
+        
+    })
+})
