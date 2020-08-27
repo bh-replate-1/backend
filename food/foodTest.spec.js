5721a916f620307184dd680043ba74f0d97b6a8c
RohithSachdeva <rsachdeva@ucdavis.edu>
2020-08-27 09:34:40 -0700

    Tests for the Food, Users, Auth

--- food/foodTest.spec.js
+++ food/foodTest.spec.js
@@ -3,45 +3,64 @@ const server = require("../api/server.js");
 const db = require("../data/dbConfig.js");
 
 //get
-//getById
-//put .. id
+//post
 //delete .. id 
-
-
-// let token;
-// beforeAll((done) => {
-//     request(server)
-//       .post('/api/auth/login')
-//       .send({ email: "Replate15", password: "Test" })
-//       .end((err, response) => {
-//         token = response.body.token; // save the token!
-//         done();
-//       });
-//   });
-
-    
-
-
-
-
 
 
 describe("Food Router Testing", () => {
     it("Returns correct unauthorized status code", () => {
         return request(server).get('/api/food')
-        .then(data => {
-            expect(data.status).toEqual(401)
-        })
+            .then(res => {
+                expect(res.status).toEqual(401)
+            })
     });
 
-    it("It responds with JSON", async () => {
+
+    it("Gets access to food list", async () => {
         const login = await request(server)
-        .post("/api/auth/login")
-        .send({ email: "Replate5", password: "Test" })
+            .post("/api/auth/login")
+            .send({ email: "Replate5", password: "Test" })
         const res = await request(server)
-        .get('/api/food')
-        .set('Authorization', login.body.token)
+            .get('/api/food')
+            .set('Authorization', login.body.token)
         expect(res.status).toEqual(200)
-        
+        expect(res.type).toBe("application/json")
+    })
+
+    it("Posts a new food item", async () => {
+        const login = await request(server)
+            .post("/api/auth/login")
+            .send({ email: "Replate5", password: "Test" })
+        const res = await request(server)
+            .post('/api/food')
+            .send({
+                food_item: "TEST!",
+                quantity: 1,
+                use_by_date: "8/30/20",
+                refrigerate: 1,
+                completed: 0,
+                user_id: 1
+            })
+            .set('Authorization', login.body.token)
+        expect(res.status).toEqual(201) 
+    })
+
+    //Delete test assumes the previously posted item is id: 1; so beforeall truncate for this (and all) test suite would be good 
+
+    //OUTDATED TEST .. checks for toggle complete
+
+    it("Toggles a food item as completed", async () => {
+        const login = await request(server)
+            .post("/api/auth/login")
+            .send({ email: "Replate5", password: "Test" })
+        const res = await request(server)
+            .delete('/api/food/6')
+            .send({
+                food_item: "Salad",
+                use_by_date: "8/30/20",
+                user_id: 1
+            })
+            .set('Authorization', login.body.token)
+        expect(res.body.completed).toBe(1) 
     })
 })
--- users/usersTest.spec.js
+++ users/usersTest.spec.js
@@ -1,3 +1,62 @@
 const request = require("supertest");
 const server = require("../api/server.js");
 const db = require("../data/dbConfig.js");
+
+//get
+//getid
+//put .. :/id
+
+
+//These tests assume there is atleast one user in the DB 
+
+describe("User Router Testing", () => {
+    it("Returns correct unauthorized status code", () => {
+        return request(server)
+            .get('/api/users')
+            .then(res => {
+                expect(res.status).toEqual(401)
+            })
+    });
+
+    it("Gets access to users list", async () => {
+        const login = await request(server)
+            .post("/api/auth/login")
+            .send({ email: "Replate5", password: "Test" })
+        const res = await request(server)
+            .get('/api/users')
+            .set('Authorization', login.body.token)
+        expect(res.status).toEqual(200)
+        expect(res.type).toBe("application/json")
+    })
+
+    it("Displays User 1's information", async () => {
+        const login = await request(server)
+            .post("/api/auth/login")
+            .send({ email: "Replate5", password: "Test" })
+        const res = await request(server)
+            .get('/api/users/1')
+            .set('Authorization', login.body.token)
+        expect(res.status).toEqual(200)
+        expect(res.body.id).toBe(1)
+    })
+
+    it("Edit User 1's information", async () => {
+        const login = await request(server)
+            .post("/api/auth/login")
+            .send({ email: "Replate5", password: "Test" })
+        const res = await request(server)
+            .put('/api/users/1')
+            .send({
+                "name": "Rohith",
+                "email": "RohithisCool11!!",
+                "address": "Hello",
+                "phone": null,
+                "company": null
+            })
+            .set('Authorization', login.body.token)
+        expect(res.status).toEqual(200)
+        expect(res.body.id).toBe(1)
+        expect(res.body.name).toBe("Rohith")
+    })
+})
+
